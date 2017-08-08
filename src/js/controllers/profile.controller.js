ad826734d2dcb809f07343e25782ae5b8f625567
tdrobinson94 <tdrobinson94@gmail.com>
2017-08-07 20:58:03 -0400

    more work to the profile page

--- src/js/controllers/profile.controller.js
+++ src/js/controllers/profile.controller.js
@@ -1,9 +1,31 @@
 import $ from 'jquery';
 
-function ProfileController(){
+function ProfileController($scope){
+    let vm = this;
 
+    vm.openAccountPopup = openAccountPopup;
+
+    function openAccountPopup(){
+        $('.popup-container').addClass('show');
+    }
+
+    $('.popup-container').click(function( event ) {
+      var target = $( event.target );
+      if (target.is(".popup-container")) {
+          //========= click outside of the card to close popup ========//
+          $('.popup-container').removeClass('show');
+      } else if (target.is(".setup-card")){
+          //========== if click is inside the card nothing will happen ========//
+      } else if (target.is(".close")){
+          //====== click the X to close popup =======//
+          $('.popup-container').removeClass('show');
+      }
+      else if(target.is(".submit-transaction")){
+          $('.popup-container').removeClass('show');
+      }
+    });
 }
 
-ProfileController.$inject = [];
+ProfileController.$inject = ['$scope'];
 
 export { ProfileController };
